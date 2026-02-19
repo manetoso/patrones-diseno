0c6290de2a60d39e76222857ae02c684378dd253
manetoso <manetoso98@hotmail.com>
2026-02-19 15:49:29 -0600

    feature(creationals): add factory function pattern

--- 01-creacionales/07-factory-function.ts
+++ 01-creacionales/07-factory-function.ts
@@ -7,3 +7,30 @@
  * * es decir, en tiempo de ejecución y no en tiempo de compilación.
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+type Language = "es" | "en" | "fr";
+
+function createGreeter(lang: Language) {
+  return function (name: string) {
+    const messages = {
+      es: `Hola, %c${name}!`,
+      en: `Hello, %c${name}!`,
+      fr: `Bonjour, %c${name}!`,
+    };
+
+    return console.log(messages[lang], COLORS.red);
+  };
+}
+
+function main() {
+  const spanishGreeter = createGreeter("es");
+  const englishGreeter = createGreeter("en");
+  const frenchGreeter = createGreeter("fr");
+
+  spanishGreeter("Fernando");
+  englishGreeter("Alise");
+  frenchGreeter("Pier");
+}
+main();
